de2e59a47a0a6adc33fa3f4246982db6db2946ce
Lucien Zimmermann <lucien.zimmermann@outlook.com>
2021-10-26 22:09:52 +0200

    Default http port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var axios = require('axios')
 var app = express()
 
-const PORT = 3000;
+const PORT = 80;
 const REDIRECT_URI = 'http://coffee-redirect.lucienzimmermann.xyz';
 const IFTTT_SERVICE_URI = 'https://maker.ifttt.com/trigger/rick_roll_coffee/with/key/lOT18SnpK41PKPK7fEUkS-4brBguqtJ_bVxs42LEqvk';
 
