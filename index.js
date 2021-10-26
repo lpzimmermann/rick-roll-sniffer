18d0c1cdc4df900c6ceb7363dc6221a48e04e803
Lucien Zimmermann <lucien.zimmermann@outlook.com>
2021-10-26 23:26:41 +0200

    redact key

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var axios = require('axios')
 var app = express()
 
 const PORT = process.env.PORT || 3000;
+const IFTTT_KEY = process.env.IFTTT_KEY || '<enter key here>';
 const REDIRECT_URI = 'http://coffee-redirect.lucienzimmermann.xyz';
-const IFTTT_SERVICE_URI = 'https://maker.ifttt.com/trigger/rick_roll_coffee/with/key/lOT18SnpK41PKPK7fEUkS-4brBguqtJ_bVxs42LEqvk';
+const IFTTT_SERVICE_URI = `https://maker.ifttt.com/trigger/rick_roll_coffee/with/key/${IFTTT_KEY}`;
 
 app.get('/', async (_, res) => {
     await axios.get(IFTTT_SERVICE_URI);
